9474260af300c4c796532e57ccf7601832bdfd3c
FurkanBekereci <frknbkrci@gmail.com>
2022-01-22 20:11:44 +0300

    Swager docs added. To run this. Change url to localhost

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express');
 const app = express();
 const authMiddleware = require('./middleware/authentication')
@@ -38,9 +44,11 @@ app.use(xss());
 
 // routes
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>Jobs Api</h1><a href="/api-docs">Documentation</a>');
+
 });
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authMiddleware, jobsRouter)
 
